d1acf02f065e62a97c9469e01f41e7b233940488
Cesar <Cesar@10.205.197.50>
2012-02-24 13:28:48 +0100

    ultima sincronizacion

--- @Note20120112/src/lector/client/reader/CommentPanel.java
+++ @Note20120112/src/lector/client/reader/CommentPanel.java
@@ -1,7 +1,15 @@
 package lector.client.reader;
 
+import lector.client.controler.Constants;
+import lector.client.reader.hilocomentarios.ReplyDialog;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.MenuItemSeparator;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RichTextArea;
@@ -20,8 +28,13 @@ public class CommentPanel extends Composite {
     private Annotation annotation;
     private VerticalPanel verticalPanel = new VerticalPanel();
     private Button button_1 = new Button("+");
-    private RichTextArea richTextAreaBoton = new RichTextArea();
+ //   private RichTextArea richTextAreaBoton = new RichTextArea();
     private SelectorPanel SE;
+    private final MenuBar menuBar = new MenuBar(false);
+    private MenuItem mntmNewItem;
+  //  private MenuItem mntmNewItem_1;
+    private final MenuItemSeparator separator = new MenuItemSeparator();
+    private MenuItem mntmNewItem_2;
     private Image Imagen;
     private Button button;
     private static boolean Estado;
@@ -39,9 +52,19 @@ public class CommentPanel extends Composite {
 
         final HorizontalPanel horizontalPanel = new HorizontalPanel();
         verticalPanel.add(horizontalPanel);
+        horizontalPanel.setHeight("28px");
 
-        button = new Button(annotation.getUserName());
-        button.setText(annotation.getUserName());
+        String Showbutton= annotation.getComment().toString();
+        if (Showbutton.length()>15){
+        	Showbutton=Showbutton.substring(0,15);
+        	Showbutton=Showbutton+" ...";
+        }
+        button = new Button(Showbutton);
+        button.setHTML(Showbutton);
+        horizontalPanel.add(button);
+        button.setEnabled(true);
+        button.setVisible(true);
+        button.setSize("254px", "30px");
         button.addMouseOutHandler(new MouseOutHandler() {
 
             public void onMouseOut(MouseOutEvent event) {
@@ -65,20 +88,28 @@ public class CommentPanel extends Composite {
             }
         });
         horizontalPanel.add(button);
-        button.setEnabled(true);
-        button.setVisible(false);
-        button.setSize("254px", "42px");
+//        button.setEnabled(true);
+//        button.setVisible(false);
+//        button.setSize("254px", "42px");
 
-        richTextAreaBoton.addClickHandler(new ClickHandler() {
+        richTextArea.addClickHandler(new ClickHandler() {
 
             public void onClick(ClickEvent event) {
-                TextComentEdit TCE = new TextComentEdit(annotation,SE);
-                TCE.center();
+                if (annotation.isEditable())
+                	{
+                	TextComentEdit TCE = new TextComentEdit(annotation,SE);
+                	TCE.center();
+                	}
+                else {
+                	TextComentNoEdit TCE= new TextComentNoEdit(annotation, SE);
+                	TCE.center();
+                }
+                
 
             }
         });
 
-        richTextAreaBoton.addMouseOutHandler(new MouseOutHandler() {
+        richTextArea.addMouseOutHandler(new MouseOutHandler() {
 
             public void onMouseOut(MouseOutEvent event) {
             	if (!Estado){
@@ -89,7 +120,7 @@ public class CommentPanel extends Composite {
             }
         });
 
-        richTextAreaBoton.addMouseOverHandler(new MouseOverHandler() {
+        richTextArea.addMouseOverHandler(new MouseOverHandler() {
 
             public void onMouseOver(MouseOverEvent event) {
             	if (!Estado){
@@ -105,12 +136,12 @@ public class CommentPanel extends Composite {
                 }
             }
         });
-
-        richTextAreaBoton.setHTML(annotation.getComment().toString());
-        richTextAreaBoton.setSize("254px", "38px");
-        horizontalPanel.add(richTextAreaBoton);
-        richTextAreaBoton.setEnabled(false);
-        richTextAreaBoton.setVisible(true);
+//
+//        richTextAreaBoton.setHTML(annotation.getComment().toString());
+//        richTextAreaBoton.setSize("254px", "38px");
+//        horizontalPanel.add(richTextAreaBoton);
+//        richTextAreaBoton.setEnabled(false);
+//        richTextAreaBoton.setVisible(true);
 
 
         button_1.addClickHandler(new ClickHandler() {
@@ -119,39 +150,61 @@ public class CommentPanel extends Composite {
                 if (button_1.getText().contentEquals("+")) {
                     //verticalPanel.add(richTextArea);
                     richTextArea.setVisible(true);
-                    button.setVisible(true);
-                    richTextAreaBoton.setVisible(false);
+                    menuBar.setVisible(true);
+                  //  button.setVisible(true);
+//                    richTextAreaBoton.setVisible(false);
                     button_1.setText("-");
                 } else {
                     // verticalPanel.remove(richTextArea);
                     richTextArea.setVisible(false);
-                    button.setVisible(false);
-                    richTextAreaBoton.setVisible(true);
-                    richTextAreaBoton.setSize("254px", "38px");
+                   // button.setVisible(false);
+//                    richTextAreaBoton.setVisible(true);
+//                    richTextAreaBoton.setSize("254px", "38px");
                     button_1.setText("+");
-                    horizontalPanel.clear();
-                    horizontalPanel.add(button);
-                    horizontalPanel.add(richTextAreaBoton);
-                    horizontalPanel.add(button_1);
+                    menuBar.setVisible(false);
+//                    horizontalPanel.clear();
+//                    horizontalPanel.add(button);
+////                    horizontalPanel.add(richTextAreaBoton);
+//                    horizontalPanel.add(button_1);
 
                 }
             }
         });
         horizontalPanel.add(button_1);
-        button_1.setSize("52px", "42px");
-
-        richTextArea.addClickHandler(new ClickHandler() {
-
-            public void onClick(ClickEvent event) {
-                TextComentEdit TCE = new TextComentEdit(annotation,SE);
-                TCE.center();
-            }
-        });
+        button_1.setSize("52px", "30px");
 
         richTextArea.setHTML(annotation.getComment().toString());
         richTextArea.setHeight("174px");
         verticalPanel.add(richTextArea);
         richTextArea.setEnabled(false);
+        
+        verticalPanel.add(menuBar);
+        
+        mntmNewItem = new MenuItem("New item", false, new Command() {
+        	public void execute() {
+        	ReplyDialog RD=new ReplyDialog(Constants.THREADFATHERNULLID, annotation.getId());
+        	RD.center();
+        	}
+        });
+        mntmNewItem.setHTML("Reply");
+        menuBar.addItem(mntmNewItem);
+        
+//        mntmNewItem_1 = new MenuItem("New item", false, new Command() {
+//        	public void execute() {
+//        		Window.alert("Borrar");
+//        	}
+//        });
+        menuBar.setVisible(false);
+//        mntmNewItem_1.setEnabled(false);
+//        mntmNewItem_1.setHTML("Delete");
+//        menuBar.addItem(mntmNewItem_1);
+        
+        menuBar.addSeparator(separator);
+        
+        mntmNewItem_2 = new MenuItem("New item", false, (Command) null);
+        menuBar.addItem(mntmNewItem_2);
+       
+       mntmNewItem_2.setText(annotation.getUserName() + " --- " +annotation.getCreatedDate().toGMTString());
         richTextArea.setVisible(false);
 
 
