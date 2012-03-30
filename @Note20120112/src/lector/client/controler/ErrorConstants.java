1e5fb0c8eff6a7d178ef862d8b47aad3bf48a395
Cesar <Cesar@Cesar-PC.home>
2012-03-30 17:37:52 +0200

    Clases Nuevas Constants

---
+++ @Note20120112/src/lector/client/controler/ErrorConstants.java
@@ -0,0 +1,7 @@
+package lector.client.controler;
+
+public class ErrorConstants {
+
+	public static final String ERROR_LOAD_FORCE_LOGIN ="Error Loadig the User, reload the web to view the changes";
+
+}
---
+++ @Note20120112/src/lector/client/controler/InformationConstants.java
@@ -0,0 +1,7 @@
+package lector.client.controler;
+
+public class InformationConstants {
+
+	public static String THIS_IS_A_LOCAL_BOOK = "This is a local book, can't be shown with google view";
+
+}
